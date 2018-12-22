ff04a12fd0148fcdb62c9e665ecadd06f999b34c
Alexander Shiyan <shc_work@mail.ru>
2018-12-22 13:51:56 +0300

    ARM i.MX21: Add devicetree support
    This patch adds basic devicetree support for i.MX21 based SoCs.
    
    Signed-off-by: Alexander Shiyan <shc_work@mail.ru>
    Signed-off-by: Ben Wolsieffer <benwolsieffer@gmail.com>

---
+++ arch/arm/mach-imx/mach-imx21.c
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/* Author: Alexander Shiyan <shc_work@mail.ru> */
+
+#include <asm/mach/arch.h>
+#include <asm/mach/map.h>
+#include <linux/of_platform.h>
+
+#include "common.h"
+#include "hardware.h"
+
+#define MX21_AVIC_ADDR	0x10040000
+
+static void __init imx21_init_irq(void)
+{
+	void __iomem *avic = ioremap(MX21_AVIC_ADDR, SZ_4K);
+
+	WARN_ON(!avic);
+
+	mxc_init_irq(avic);
+}
+
+static const char * const imx21_dt_board_compat[] __initconst = {
+	"fsl,imx21",
+	NULL
+};
+
+DT_MACHINE_START(IMX21_DT, "Freescale i.MX21 (Device Tree Support)")
+	.init_early	= imx21_init_early,
+	.init_irq	= imx21_init_irq,
+	.dt_compat	= imx21_dt_board_compat,
+MACHINE_END
